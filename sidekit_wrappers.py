d83802c6f135ce0d0481c5018181af43deca9660
Anthony Larcher <anthony.larcher@univ-lemans.fr>
2015-11-25 08:18:52 +0100

    test

--- sidekit_wrappers.py
+++ sidekit_wrappers.py
@@ -99,9 +99,8 @@ def process_parallel_lists(func):
     """
     def wrapper(*args, **kwargs):
         
-        if len(args) > 0:
+        if len(args) > 1:
             print("Warning, some arguments are not named, computation might not be parallelized")
-            print("args = {}".format(args))
         
         numThread = 1
         if "numThread" in kwargs.keys():
