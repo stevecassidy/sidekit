- correct warring introduce with numpy 1.11 and pandas - hdf5 output for mixture, TV, plda, normalization - ...

features_server.py
@@ -186,6 +186,7 @@ class FeaturesServer:
         self.keep_all_features = False
         self.spec = False
         self.mspec = False
+        self.hdf5_fh = None
 
         # If a predefined config is chosen, apply it
         if config == 'diar_16k':
@@ -445,7 +446,7 @@ class FeaturesServer:
         if rate != self.sampling_frequency:
             raise "file rate don't match the rate of the feature server configuration"
         self.audio_filename = audio_filename
-        logging.info(' size of signal: %f len %d type size %d', x.nbytes/1024/1024, len(x), x.nbytes/len(x))
+        logging.info('size of signal: %f len %d type size %d', x.nbytes/1024/1024, len(x), x.nbytes/len(x))
 
         if x.ndim == 1:
             x = x[:, np.newaxis]
@@ -479,7 +480,6 @@ class FeaturesServer:
                              l / self.sampling_frequency)
 
                 tmp = self._features_chan(show, channel_ext, x[start:end, chan])
-
                 if cep is None:
                     cep = []
                     label = []
@@ -491,8 +491,9 @@ class FeaturesServer:
                 start = end - dec2
                 end = min(end + dec, l)
                 if cep[-1].shape[0] > 0:
-                    logging.info('!! size of signal cep: %f len %d type size %d', cep[-1].nbytes/1024/1024, len(cep[-1]),
-                             cep[-1].nbytes/len(cep[-1]))
+                    l = len(cep[-1])
+                    logging.info('size of signal cep: %f len %d type size %d', cep[-1].nbytes/1024/1024, l,
+                             cep[-1].nbytes/l)
         del x
         # Smooth the labels and fuse the channels if more than one.
         logging.info('Smooth the labels and fuse the channels if more than one')
@@ -503,7 +504,7 @@ class FeaturesServer:
         # Keep only the required features and save the appropriate files
         # which are either feature files alone or feature and label files
         if not self.keep_all_features:
-            logging.debug('no keep all')
+            logging.info('no keep all')
             for chan, chan_ext in enumerate(channel_ext):
                 cep[chan] = cep[chan][label[chan]]
                 label[chan] = label[chan][label[chan]]
@@ -702,6 +703,13 @@ class FeaturesServer:
                 input_filename = os.path.join(self.input_dir.format(s=show),
                                               show + self.input_file_extension)
                 self.cep = [read_htk(input_filename)[0]]
+            elif self.from_file == 'hdf5':
+                logging.debug('load hdf5: ' + show)
+                input_filename = os.path.join(self.input_dir + self.input_file_extension)
+                hdf5_input_fh = h5py.File(input_filename, "r")
+                self.cep = [read_cep_hdf5(hdf5_input_fh, show)]
+                hdf5_input_fh.close()
+
             else:
                 raise Exception('unknown from_file value')
 
@@ -803,6 +811,17 @@ class FeaturesServer:
                     write_htk(self.cep[0], filename[0])
                 if self.cep[1].shape[0] > 0:
                     write_htk(self.cep[1], filename[1])
+        elif self.from_file == 'hdf5':
+            hdf5_ouput_fh = h5py.File(filename, "w")
+            if len(self.cep) == 1 and self.cep[0].shape[0] > 0:
+                logging.debug('save hdf5: ' + show)
+                write_cep_hdf5(self.cep[0], hdf5_ouput_fh, show)
+            elif len(self.cep) == 2:
+                logging.info('save htk format: %s', show, self.double_channel_extension[0])
+                logging.info('save htk format: %s', show, self.double_channel_extension[1])
+                write_cep_hdf5(self.cep[0], hdf5_ouput_fh, show+'/'+self.double_channel_extension[0])
+                write_cep_hdf5(self.cep[0], hdf5_ouput_fh, show+'/'+self.double_channel_extension[1])
+            hdf5_ouput_fh.close()
         else:
             raise Exception('unknown feature format')
 

frontend/features.py
@@ -349,6 +349,7 @@ def mfcc(input_sig,
     # Prepare the signal to be processed on a sliding window.
     # We first compute the overlap of frames and cut the signal in frames of length nwin
     # overlaping by "overlap" samples
+
     window_length = int(round(nwin * fs))
     overlap = window_length - int(shift * fs)
     framed = framing(input_sig, window_length, win_shift=window_length-overlap).copy()
@@ -359,7 +360,7 @@ def mfcc(input_sig,
     l = framed.shape[0]
     nfft = 2 ** int(np.ceil(np.log2(window_length)))
     ham = np.hamming(window_length)
-    spec = np.ones((l, nfft / 2 + 1))
+    spec = np.ones((l, int(nfft / 2) + 1))
     logEnergy = np.log((framed**2).sum(axis=1))
 
     dec = 10000
@@ -410,7 +411,7 @@ def framing(sig, win_size, win_shift=1, context=(0,0), pad='zeros'):
     # Manage padding
     c = (context,) +  (sig.ndim - 1) * ((0,0),)
     _win_size = win_size + sum(context)
-    shape = ((sig.shape[0] - win_size) / win_shift + 1, 1, _win_size, sig.shape[1])
+    shape = (int((sig.shape[0] - win_size) / win_shift) + 1, 1, _win_size, sig.shape[1])
     strides = tuple(map(lambda x: x * dsize, [win_shift * sig.shape[1], 1, sig.shape[1], 1]))
     return np.lib.stride_tricks.as_strided(np.lib.pad(sig, c, 'constant', constant_values=(0,)),
                                                     shape=shape,

frontend/normfeat.py
@@ -136,7 +136,7 @@ def cmvn_test(features, label=[], on_label=False):
         mu = np.mean(features[label, :], axis=0)
         stdev = np.std(features[label, :], axis=0)
 
-        if on_label:
+        if not on_label:
             features -= mu
             features /= stdev
         else:
@@ -221,12 +221,15 @@ def cep_sliding_norm(cep, win=301, center=True, reduce=False):
     dwin = win // 2
 
     df = pd.DataFrame(cep)
+    r = df.rolling(window=win, center=True)
+    mean = r.mean().values
+    std = r.std().values
 
-    mean = pd.rolling_mean(df, win, center=True).values
+    #mean = pd.rolling_mean(df, win, center=True).values
     mean[0:dwin,:] = mean[dwin,:]
     mean[-dwin:,:] = mean[-dwin-1,:]
 
-    std = pd.rolling_std(df, win, center=True).values
+    #std = pd.rolling_std(df, win, center=True).values
     std[0:dwin,:] = std[dwin,:]
     std[-dwin:,:] = std[-dwin-1,:]
 

frontend/vad.py
@@ -191,10 +191,10 @@ def speech_enhancement(X, Gain, Noise_floor, Fs, Ascale, NN):
 
     ###################################################################
     # initial parameter for noise min
-    mb = np.ones((1 + FrameSize / 2, 4)) * FrameSize / 2  # 129x4  set four buffer * FrameSize/2
+    mb = np.ones((1 + int(FrameSize / 2), 4)) * FrameSize / 2  # 129x4  set four buffer * FrameSize/2
     im = 0
     Beta1 = 0.9024  # seems that small value is better;
-    pxn = np.zeros(1 + FrameSize / 2)  # 1+FrameSize/2=129 zeros vector
+    pxn = np.zeros(1 + int(FrameSize / 2))  # 1+FrameSize/2=129 zeros vector
 
     ###################################################################
     old_absx = Eabsn
@@ -324,6 +324,9 @@ def speech_enhancement(X, Gain, Noise_floor, Fs, Ascale, NN):
     # }
     return X1
 
+def vad_percentil(logEnergy, percent):
+    thr = np.percentile(logEnergy, percent)
+    return logEnergy > thr, thr
 
 def vad_energy(logEnergy,
                distribNb=3,
@@ -361,7 +364,7 @@ def vad_energy(logEnergy,
 
     # Apply frame selection with the current threshold
     label = logEnergy > threshold
-    return label
+    return label, threshold
 
 
 def vad_snr(sig, snr, fs=16000, shift=0.01, nwin=256):

mixture.py
@@ -38,7 +38,6 @@ import gzip
 import logging
 import os
 import warnings
-
 from sidekit.sidekit_wrappers import *
 
 if sys.version_info.major == 3:
@@ -207,20 +206,20 @@ class Mixture(object):
         else:
             raise Exception('Error: unknown extension')
             
-    def read_hdf5(self, mixtureFileName):
+    def read_hdf5(self, mixtureFileName_or_fh, prefix=''):
         """Read a Mixture in hdf5 format
 
         :param mixtureFileName: name of the file to read from
         """
-        with h5py.File(mixtureFileName, 'r') as f:
-            self.w = f.get('/w').value
+        with h5py.File(mixtureFileName_or_fh, 'r') as f:
+            self.w = f.get(prefix+'/w').value
             self.w.resize(np.max(self.w.shape))
-            self.mu = f.get('/mu').value
-            self.invcov = f.get('/invcov').value
-            self.cov_var_ctl = f.get('/cov_var_ctl').value
-            self.cst = f.get('/cst').value
-            self.det = f.get('/det').value
-            self.A = f.get('/A').value
+            self.mu = f.get(prefix+'/mu').value
+            self.invcov = f.get(prefix+'/invcov').value
+            self.cov_var_ctl = f.get(prefix+'/cov_var_ctl').value
+            self.cst = f.get(prefix+'/cst').value
+            self.det = f.get(prefix+'/det').value
+            self.A = f.get(prefix+'/A').value
 
     def read_pickle(self, inputFileName):
         """Read IdMap in PICKLE format.
@@ -323,9 +322,13 @@ class Mixture(object):
                     self.cst[distrib] = np.exp(-.05 * np.double(w[1]))
         self._compute_all()
 
-    @check_path_existance
+    @deprecated
     def save(self, outputFileName):
-        """Save the Mixture object to file. The format of the file 
+        self.write(outputFileName)
+
+    @check_path_existance
+    def write(self, outputFileName):
+        """Save the Mixture object to file. The format of the file
         to create is set accordingly to the extension of the filename.
         This extension can be '.p' for pickle format, '.hdf5' and '.h5' 
         for HDF5 format, '.gmm' for ALIZE format (HTK not implemented yet)
@@ -334,19 +337,23 @@ class Mixture(object):
         """
         extension = os.path.splitext(outputFileName)[1][1:].lower()
         if extension == 'p':
-            self.save_pickle(outputFileName)
+            self.write_pickle(outputFileName)
         elif extension in ['hdf5', 'h5']:
             if h5py_loaded:
-                self.save_hdf5(outputFileName)
+                self.write_hdf5(outputFileName)
             else:
                 raise Exception('h5py is not installed, chose another' + ' format to load your IdMap')
         elif extension == 'gmm':
-            self.save_alize(outputFileName)
+            self.write_alize(outputFileName)
         else:
             raise Exception('Wrong output format, must be pickle or hdf5')
 
-    @check_path_existance
+    @deprecated
     def save_alize(self, mixtureFileName):
+        self.write_alize(mixtureFileName)
+
+    @check_path_existance
+    def write_alize(self, mixtureFileName):
         """Save a mixture in alize raw format
 
         :param mixtureFileName: name of the file to write in     
@@ -372,40 +379,50 @@ class Mixture(object):
                 # Means
                 of.write(struct.pack("<" + "d" * self.dim(), *self.mu[d, :]))
 
+    @deprecated
+    def save_hdf5(self, mixtureFileName, prefix=''):
+        self.write_hdf5(mixtureFileName, prefix)
+
+
     @check_path_existance
-    def save_hdf5(self, mixtureFileName):
+    def write_hdf5(self, mixtureFileName, prefix=''):
         """Save a Mixture in hdf5 format
 
         :param mixtureFileName: the name of the file to write in
         """
         f = h5py.File(mixtureFileName, 'w')
-        f.create_dataset('/w', self.w.shape, "d", self.w,
+
+        f.create_dataset(prefix+'/w', self.w.shape, "d", self.w,
                          compression="gzip",
                          fletcher32=True)
-        f.create_dataset('/mu', self.mu.shape, "d", self.mu,
+        f.create_dataset(prefix+'/mu', self.mu.shape, "d", self.mu,
                          compression="gzip",
                          fletcher32=True)
-        f.create_dataset('/invcov', self.invcov.shape, "d", self.invcov,
+        f.create_dataset(prefix+'/invcov', self.invcov.shape, "d", self.invcov,
                          compression="gzip",
                          fletcher32=True)
-        f.create_dataset('/cov_var_ctl', self.cov_var_ctl.shape, "d", 
+        f.create_dataset(prefix+'/cov_var_ctl', self.cov_var_ctl.shape, "d",
                          self.cov_var_ctl,
                          compression="gzip",
                          fletcher32=True)
-        f.create_dataset('/cst', self.cst.shape, "d", self.cst,
+        f.create_dataset(prefix+'/cst', self.cst.shape, "d", self.cst,
                          compression="gzip",
                          fletcher32=True)
-        f.create_dataset('/det', self.det.shape, "d", self.det,
+        f.create_dataset(prefix+'/det', self.det.shape, "d", self.det,
                          compression="gzip",
                          fletcher32=True)
-        f.create_dataset('/A', self.A.shape, "d", self.A,
+        f.create_dataset(prefix+'/A', self.A.shape, "d", self.A,
                          compression="gzip",
                          fletcher32=True)
         
         f.close()
 
-    @check_path_existance
+    @deprecated
     def save_pickle(self, outputFileName):
+        self.write_pickle(outputFileName)
+
+    @check_path_existance
+    def write_pickle(self, outputFileName):
         """Save Ndx in PICKLE format. Convert all data into float32 
         before saving, note that the conversion doesn't apply in Python 2.X
         
@@ -420,8 +437,12 @@ class Mixture(object):
             self.det.astype('float32', copy=False)
             pickle.dump(self, f)
 
-    @check_path_existance
+    @deprecated
     def save_htk(self, mixtureFileName):
+        self.write_htk(mixtureFileName)
+
+    @check_path_existance
+    def write_htk(self, mixtureFileName):
         """Save a Mixture in HTK format
         
         :param mixtureFileName: the name of the file to write in

sidekit_io.py
@@ -148,102 +148,126 @@ def write_pickle(obj, filename):
     with gzip.open(filename, 'wb') as f:
         pickle.dump(obj, f)
 
+@check_path_existance
+def write_tv_hdf5(data, outpuFileName):
+    tv = data[0]
+    tv_mean = data[1]
+    tv_sigma = data[2]
+    d = dict()
+    d['tv/tv'] = tv
+    d['tv/tv_mean'] = tv_mean
+    d['tv/tv_sigma'] = tv_sigma
+    write_dict_hdf5(d, outpuFileName)
+
+def read_tv_hdf5(inputFileName):
+    with h5py.File(inputFileName, "r") as f:
+        tv = f.get("tv/tv").value
+        tv_mean = f.get("tv/tv_mean").value
+        tv_sigma = f.get("tv/tv_sigma").value
+    return tv, tv_mean, tv_sigma
+
+@check_path_existance
 def write_dict_hdf5(data, outpuFileName):
     with h5py.File(outpuFileName, "w") as f:
         for key in data:
             value = data[key]
-            #print(type(value))
             if isinstance(value, np.ndarray) or isinstance(value, list):
                 f.create_dataset(key, data=value, compression="gzip", fletcher32=True)
             else:
                 f.create_dataset(key, data=value)
 
-def read_dict_hdf5(outpuFileName):
+def read_dict_hdf5(inputFileName):
     data = dict()
-    with h5py.File(outpuFileName, "r") as f:
+    with h5py.File(inputFileName, "r") as f:
         for key in f.keys():
             data[key] = f.get(key).value
     return data
 
-# def write_norm_hdf5(means, covs, outpuFileName):
-#     with h5py.File(outpuFileName, "w") as f:
-#         a = len(means)
-#         f.create_dataset("len", data=a,
-#                          maxshape=(None,))
-#         for i in range(a):
-#             f.create_dataset("mean"+str(i), data=means[i],
-#                              maxshape=(None,),
-#                              compression="gzip",
-#                              fletcher32=True)
-#             f.create_dataset("cov"+str(i), data=covs[i],
-#                              maxshape=(None, None),
-#                              compression="gzip",
-#                              fletcher32=True)
-#
-# def read_norm_hdf5(statserverFileName):
-#     with h5py.File(statserverFileName, "r") as f:
-#         means = list()
-#         covs = list()
-#
-#         l = f.get("len").value
-#         for i in range(int(l)):
-#             means.append(f.get("mean"+str(i)).value)
-#             covs.append(f.get("cov"+str(i)).value)
-#         return means, covs
-#
-# def write_fa_hdf5(mean, F, G, H, Sigma, outpuFileName):
-#     with h5py.File(outpuFileName, "w") as f:
-#         kind = np.zeros(5, dtype="int16") # FA with 5 matrix
-#         if mean is not None:
-#             kind[0] = 1
-#             f.create_dataset("mean", data=mean,
-#                              maxshape=(None,),
-#                              compression="gzip",
-#                              fletcher32=True)
-#         if F is not None:
-#             kind[1] = 1
-#             f.create_dataset("F", data=F,
-#                              maxshape=(None, None),
-#                              compression="gzip",
-#                              fletcher32=True)
-#         if G is not None:
-#             kind[2] = 1
-#             f.create_dataset("G", data=G,
-#                              maxshape=(None, None),
-#                              compression="gzip",
-#                              fletcher32=True)
-#         if H is not None:
-#             kind[3] = 1
-#             f.create_dataset("H", data=H,
-#                              maxshape=(None, None),
-#                              compression="gzip",
-#                              fletcher32=True)
-#         if Sigma is not None:
-#             kind[4] = 1
-#             f.create_dataset("Sigma", data=Sigma,
-#                              maxshape=(None, None),
-#                              compression="gzip",
-#                              fletcher32=True)
-#         f.create_dataset("kind", data=kind,
-#                          maxshape=(None,),
-#                          compression="gzip",
-#                          fletcher32=True)
-#
-# def read_fa_hdf5(statserverFileName):
-#     with h5py.File(statserverFileName, "r") as f:
-#         kind = f.get("kind").value
-#         mean = F = G = H = Sigma = None
-#         if kind[0] != 0:
-#             mean = f.get("mean").value
-#         if kind[1] != 0:
-#             F = f.get("F").value
-#         if kind[2] != 0:
-#             G = f.get("G").value
-#         if kind[3] != 0:
-#             H = f.get("H").value
-#         if kind[4] != 0:
-#             Sigma = f.get("Sigma").value
-#         return mean, F, G, H, Sigma
+# def update_or_create_hdf5(fileHandler, key, value):
+#     if key in fileHandler:
+#         data = fileHandler[key]
+#         data.resize(value.shape)
+#         data[...] = value
+#         print(value)
+#         print(fileHandler.get(key).value)
+#     else:
+#         if isinstance(value, np.ndarray) or isinstance(value, list):
+#             fileHandler.create_dataset(key, data=value, compression="gzip", fletcher32=True)
+#         else:
+#             fileHandler.create_dataset(key, data=value)
+
+
+@check_path_existance
+def write_norm_hdf5(data, outpuFileName):
+    with h5py.File(outpuFileName, "w") as f:
+        means = data[0]
+        covs = data[1]
+        f.create_dataset("norm/means", data=means,
+                         compression="gzip",
+                         fletcher32=True)
+        f.create_dataset("norm/covs", data=covs,
+                         compression="gzip",
+                         fletcher32=True)
+
+def read_norm_hdf5(statserverFileName):
+    with h5py.File(statserverFileName, "r") as f:
+        means = f.get("norm/means").value
+        covs = f.get("norm/covs").value
+    return means, covs
+
+@check_path_existance
+def write_fa_hdf5(data, outpuFileName):
+    mean = data[0]
+    F = data[1]
+    G = data[2]
+    H = data[3]
+    Sigma = data[4]
+    with h5py.File(outpuFileName, "w") as f:
+        kind = np.zeros(5, dtype="int16") # FA with 5 matrix
+        if mean is not None:
+            kind[0] = 1
+            f.create_dataset("fa/mean", data=mean,
+                             compression="gzip",
+                             fletcher32=True)
+        if F is not None:
+            kind[1] = 1
+            f.create_dataset("fa/f", data=F,
+                             compression="gzip",
+                             fletcher32=True)
+        if G is not None:
+            kind[2] = 1
+            f.create_dataset("fa/g", data=G,
+                             compression="gzip",
+                             fletcher32=True)
+        if H is not None:
+            kind[3] = 1
+            f.create_dataset("fa/h", data=H,
+                             compression="gzip",
+                             fletcher32=True)
+        if Sigma is not None:
+            kind[4] = 1
+            f.create_dataset("fa/sigma", data=Sigma,
+                             compression="gzip",
+                             fletcher32=True)
+        f.create_dataset("fa/kind", data=kind,
+                         compression="gzip",
+                         fletcher32=True)
+
+def read_fa_hdf5(statserverFileName):
+    with h5py.File(statserverFileName, "r") as f:
+        kind = f.get("fa/kind").value
+        mean = F = G = H = Sigma = None
+        if kind[0] != 0:
+            mean = f.get("fa/mean").value
+        if kind[1] != 0:
+            F = f.get("fa/f").value
+        if kind[2] != 0:
+            G = f.get("fa/g").value
+        if kind[3] != 0:
+            H = f.get("fa/h").value
+        if kind[4] != 0:
+            Sigma = f.get("fa/sigma").value
+        return mean, F, G, H, Sigma
 
 def init_logging(level=logging.INFO, filename=None):
     np.set_printoptions(linewidth=250, precision=4)